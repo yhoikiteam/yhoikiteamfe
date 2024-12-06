ed7d5578e74248341d53782891a874948871c078
ProjectAhkdan <ahkdanprika1@gmail.com>
2024-12-06 14:56:34 +0700

    Hero Section

---
+++ app/Herosection/Data/User.tsx
@@ -0,0 +1,28 @@
+export const users = [
+    {
+      name: "Kipin Kipon",
+      description: "Kula asale saking ngayogyakarta omagaaaaa",
+      imagePath: "/Herosection/Profile.png", 
+    },
+    {
+      name: "Kipin Kipon",
+      description: "Kula asale saking ngayogyakarta omagaaaaa",
+      imagePath: "/Herosection/Profile.png", 
+    },
+    {
+      name: "Kipin Kipon",
+      description: "Kula asale saking ngayogyakarta omagaaaaa",
+      imagePath: "/Herosection/Profile.png", 
+    },
+    {
+      name: "Kipin Kipon",
+      description: "Kula asale saking ngayogyakarta omagaaaaa",
+      imagePath: "/Herosection/Profile.png", 
+    },
+    {
+      name: "Kipin Kiponr",
+      description: "Kula asale saking ngayogyakarta omagaaaaa",
+      imagePath: "/Herosection/Profile.png", 
+    },
+  ];
+  
