ab8ad8ef64b7d1b83e00edf1fdbd5672b0323409
ProjectAhkdan <ahkdanprika1@gmail.com>
2024-12-06 16:49:21 +0700

    hero Section update

--- app/Herosection/Data/User.tsx
+++ app/Herosection/Data/User.tsx
@@ -1,4 +1,10 @@
-export const users = [
+interface User {
+  name: string;
+  description: string;
+  imagePath: string;
+}
+
+export const users: User[] = [
     {
       name: "Kipin Kipon",
       description: "Kula asale saking ngayogyakarta omagaaaaa",
